b060576305b7ad4ff83e29448957a47925b60b76
Naveen4272 <naveenkumar4272n@gmail.com>
2025-06-08 02:59:54 +0530

    rotuing and request handler

--- src/app.js
+++ src/app.js
@@ -2,12 +2,27 @@ const express=require("express");
 
 const app=express();
 
+app.use("/hello/123",(req,res)=>{
+  res.send("hello123")
+})
+
 app.use("/hello",(req,res)=>{
   res.send("hello world")
 })
 
+app.get("/user",(req,res)=>{
+  res.send("user")
+})
+app.post("/user",(req,res)=>{
+  res.send("user data")
+})
 
 
+
+
+app.use("/",(req,res)=>{
+  res.send("hi")
+})
 app.listen(3000,()=>{
   console.log("server listening to the port 3000...! ");
   
